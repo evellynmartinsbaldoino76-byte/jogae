482bf4b0ecd8d5109bd78a49b2dc858ae5d53d16
Evellyn Martins <evellynmartinsbaldoino76@gmail.com>
2026-07-22 16:22:02 -0300

    Adiciona menu mobile

--- src/components/layout/DashboardShell.tsx
+++ src/components/layout/DashboardShell.tsx
@@ -1,77 +1,57 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+"use client";
+
+import {
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 import { AppSidebar } from "./sidebar/AppSidebar";
-
 
 export function DashboardShell({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-
   return (
-
     <SidebarProvider>
-
 
       <div
         className="
-        flex
-        min-h-screen
-        w-full
-        bg-gradient-to-br
-        from-[#061a16]
-        via-[#071525]
-        to-[#05070d]
+          flex
+          min-h-screen
+          w-full
+          bg-gradient-to-br
+          from-[#061a16]
+          via-[#071525]
+          to-[#05070d]
         "
       >
 
-
-
         <AppSidebar />
 
+        <main className="flex-1">
 
-
-
-
-        <main
-
-          className="
-          flex-1
-          overflow-hidden
-          "
-
-        >
-
-
-
-          <div
-
+          <header
             className="
-            p-6
+              flex
+              h-16
+              items-center
+              border-b
+              border-white/10
+              px-4
             "
-
           >
+            <SidebarTrigger />
+          </header>
 
+          <div className="p-6">
             {children}
-
-
           </div>
-
-
 
         </main>
 
-
-
-
       </div>
 
-
-
     </SidebarProvider>
-
   );
-
 }
